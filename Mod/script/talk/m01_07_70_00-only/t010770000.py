bc9e93eb01e1d1535c68130243b4b81bce507aec
Vawser <ben.rouncefield@gmail.com>
2024-10-31 22:09:15 +0000

    Commit

--- Mod/script/talk/m01_07_70_00-only/t010770000.py
+++ Mod/script/talk/m01_07_70_00-only/t010770000.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 #-------------------------------
-# Start
+# Menu
 #-------------------------------
 def t010770000_0():
-    # Player is dead
+    # Player Death
     if IsPlayerDead() == 1:
         # Wait 5 seconds
         assert GetCurrentStateElapsedTime() > 5
@@ -13,7 +13,7 @@ def t010770000_0():
         
         c1_119(10)
         Quit()
-    # Termine Program menu option
+    # End Menu
     elif GetEventFlag(4591) == 1:
         SetEventFlag(4591, 0)
         EndMission(0, 0, 0)
